4cb5302631312e6b0e47b7bb1840aa829db06d0f
Sochetra NOV <sochetranov@Sochetras-MacBook-Pro.local>
2020-07-10 13:14:20 +0700

    add swipe to dismiss

--- lib/swip_dismiss.dart
+++ lib/swip_dismiss.dart
@@ -21,7 +21,19 @@ class _ListViewBuilder extends StatefulWidget {
 }
 
 class _ListViewState extends State<_ListViewBuilder> {
-  final items = List.generate(20, (index) => "Item ${index + 1}");
+  final List<SampleImage> items = <SampleImage>[
+    SampleImage(id: 0, download_url: "https://unsplash.com/photos/yC-Yzbqy7PY"),
+    SampleImage(id: 0, download_url: "https://picsum.photos/id/1/5616/3744"),
+    SampleImage(id: 0, download_url: "https://picsum.photos/id/10/2500/1667"),
+    SampleImage(id: 0, download_url: "https://picsum.photos/id/100/2500/1656"),
+    SampleImage(id: 0, download_url: "https://picsum.photos/id/1000/5626/3635"),
+    SampleImage(id: 0, download_url: "https://picsum.photos/id/1001/5616/3744"),
+    SampleImage(id: 0, download_url: "https://picsum.photos/id/1002/4312/2868"),
+    SampleImage(id: 0, download_url: "https://picsum.photos/id/1004/5616/3744"),
+    SampleImage(id: 0, download_url: "https://picsum.photos/id/1005/5760/3840"),
+    SampleImage(id: 0, download_url: "https://picsum.photos/id/1006/3000/2000"),
+    SampleImage(id: 0, download_url: "https://picsum.photos/id/1008/5616/3744")
+  ];
 
   @override
   Widget build(BuildContext context) {
@@ -31,7 +43,7 @@ class _ListViewState extends State<_ListViewBuilder> {
         final item = items[index];
 
         return Dismissible(
-            key: Key(item),
+            key: Key(item.download_url),
             onDismissed: (direction) {
               final snackbar = SnackBar(
                   content: Text("1 deleted item"),
@@ -47,12 +59,31 @@ class _ListViewState extends State<_ListViewBuilder> {
             },
             background: Container(color: Colors.red),
             child: InkWell(
-              onTap: () {
-                print("item tap");
-              },
-              child: ListTile(title: Text(item)),
-            ));
+                onTap: () {
+                  print("item tap");
+                },
+                child: ListTile(
+                    title: Row(
+                  children: <Widget>[
+                    new ClipRRect(
+                      borderRadius: BorderRadius.all(Radius.circular(5)),
+                      child: Image.network(
+                        "https://picsum.photos/250?image=9",
+                        width: 50,
+                        height: 50,
+                      ),
+                    ),
+                    Text("Row"),
+                  ],
+                ))));
       },
     );
   }
+}
+
+class SampleImage {
+  final int id;
+  final String download_url;
+
+  SampleImage({this.id, this.download_url});
 }
